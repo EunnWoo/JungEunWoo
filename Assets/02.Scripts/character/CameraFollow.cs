d0264a65679cf8a323be1523a77306cccecce0c1
EunWoo Jung <rejkh5020@naver.com>
2022-01-02 23:07:09 +0900

    main

---
+++ Assets/02.Scripts/character/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform camera;
+    void Update()
+    {
+        LookAround();
+    }
+    private void LookAround(){
+       Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"),Input.GetAxis("Mouse Y"));
+       Vector3 camAngle = camera.rotation.eulerAngles;
+       float x = camAngle.x - mouseDelta.y;
+       if(x<100f){
+           x = Mathf.Clamp(x, -1f, 20f);
+       }
+       else{
+           x = Mathf.Clamp(x, 335f, 361f);
+       }
+       camera.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
+    }
+    void LateUpdate() {
+        
+        
+    }
+}
---
+++ Assets/02.Scripts/character/CharacterMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    [SerializeField]
+    private Transform characterBody;
+    [SerializeField]
+    private Transform camera;
+    Vector3 moveVec;
+    float hAxis;
+    float vAxis;
+    bool isRun;
+    bool isBack = false;
+    Animator ani;
+    void Start()
+    {
+        ani = characterBody.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        hAxis = Input.GetAxis("Horizontal");
+        vAxis = Input.GetAxis("Vertical");
+        isRun = Input.GetButton("Run");
+        if(Input.GetKeyDown(KeyCode.S)){
+            isBack = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.S)){
+            isBack = false;
+        }
+        Vector2 moveInput = new Vector2(hAxis, vAxis) * (isRun && !isBack ? 1.3f : 1f) * (!isBack ? 1f : 0.5f);
+        bool isMove = moveInput.magnitude != 0;
+        ani.SetFloat("vAxis",vAxis,0.2f,Time.deltaTime);
+        
+        // ani.SetBool("IsWalk", isMove);
+        ani.SetBool("IsRun", isRun && !isBack);
+        // ani.SetBool("IsBack", isBack);
+        Vector3 lookForward = new Vector3(camera.forward.x, 0f, camera.forward.z).normalized;
+        Vector3 lookRight = new Vector3(camera.right.x, 0f, camera.right.z).normalized;
+        Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+        if (vAxis != 0f && hAxis != 0f)
+        {
+            if(hAxis * vAxis > 0f){
+                lookForward = Quaternion.Euler(0, 45, 0).normalized * lookForward;
+            }
+            else{
+                lookForward = Quaternion.Euler(0, -45, 0).normalized * lookForward;
+            }
+        }
+        characterBody.forward = lookForward;
+        transform.position += moveDir * Time.deltaTime * 5f;
+    }
+}
