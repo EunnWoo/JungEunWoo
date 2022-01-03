5cd154e83cac27da09b670e17cea6766a37c5e3c
EunWoo <rejkh5020@naver.com>
2022-01-03 15:49:38 +0900

    낙사 충돌

---
+++ Assets/02.Scripts/MapManager/MapManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField]
+    private BoxCollider box;
+
+    void Start()
+    {
+        box = GetComponent<BoxCollider>();
+    }
+
+   
+    void Update()
+    {
+       
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Ãæµ¹");
+        other.gameObject.transform.position = new Vector3(0, 1, 0);
+       // other.GetComponentInChildren<Transform>().position = new Vector3(0, 1, 0);
+    }
+}
