e5ce1a4d028d66fee5ecb29ca8aae8c644aa8ad2
sungrak <tjdfkr0460@naver.com>
2022-01-04 16:58:37 +0900

    구르기제한,무브 코드정리

--- Assets/02.Scripts/character/CharacterMove.cs
+++ Assets/02.Scripts/character/CharacterMove.cs
@@ -13,7 +13,7 @@ public class CharacterMove : MonoBehaviourPun
     Animator ani;
     float hAxis;
     float vAxis;
-    bool isRun, isRoll = false;
+    bool isRun, isLeft, isRight, isRoll;
     bool isBack = false;
     bool isGround;
     void Start()
@@ -23,20 +23,20 @@ public class CharacterMove : MonoBehaviourPun
     }
     void Update()
     {
-        if (!photonView.IsMine)
-        {
-            return;
-        }
+        GetInput();
         Move();
-        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true && !ani.GetCurrentAnimatorStateInfo(0).IsTag("Roll"))
+        {
             Jump();
+        }
+        Animation();
     }
-    private void Move()
+    void GetInput()
     {
         hAxis = Input.GetAxis("Horizontal");//a,d이동 값
         vAxis = Input.GetAxis("Vertical");//w,s이동 값
         isRun = Input.GetButton("Run");//left shift가 눌렸는지
-        isRoll = Input.GetButton("Roll");
+        isRoll = Input.GetButtonDown("Roll");
         if (Input.GetKeyDown(KeyCode.S))
         {
             isBack = true;
@@ -45,12 +45,31 @@ public class CharacterMove : MonoBehaviourPun
         {
             isBack = false;
         }
-        Vector2 moveInput = new Vector2(hAxis, vAxis).normalized * (isRun && !isBack ? 1.3f : 1f) * (!isBack ? 1f : 0.5f);//이동을 하고 있는지 뛰면 속도가 1.3 뒤로가고있으면 원래 속도의 0.5
+        if (Input.GetKey(KeyCode.A))
+        {
+            isLeft = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            isLeft = false;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            isRight = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            isRight = false;
+        }
+    }
+    void Move()
+    {
+        Vector2 moveInput = new Vector2(hAxis, vAxis).normalized * (isRun && !isBack ? 1.3f : 1f) * (!isBack ? 1f : 0.5f) * ((isLeft || isRight) && vAxis == 0f ? 0.7f : 1f);//이동을 하고 있는지 뛰면 속도가 1.3 뒤로가고있으면 원래 속도의 0.5
         bool isMove = moveInput.magnitude != 0;//움직이고 있는가?
         Vector3 lookForward = new Vector3(camera.forward.x, 0f, camera.forward.z).normalized;//카메라가 보는 방향
         Vector3 lookRight = new Vector3(camera.right.x, 0f, camera.right.z).normalized;
         Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
-        if (vAxis != 0f && hAxis != 0f)
+        if (vAxis != 0f && hAxis != 0f)//대각선 바라보기
         {
             if (hAxis * vAxis >= 0f)
             {
@@ -61,34 +80,19 @@ public class CharacterMove : MonoBehaviourPun
                 lookForward = Quaternion.Euler(0, -45, 0) * lookForward;
             }
         }
-        if (Input.GetKey(KeyCode.A) && vAxis == 0f)
-        {
-            moveDir *= 0.7f;
-            ani.SetBool("IsLeft", true);
-            ani.SetBool("IsLeftRoll", isRoll);
-        }
-        else if (Input.GetKeyUp(KeyCode.A) || vAxis != 0f)
-        {
-            //moveDir *= 1f;
-            ani.SetBool("IsLeft", false);
-        }
-        if (Input.GetKey(KeyCode.D) && vAxis == 0f)
-        {
-            moveDir *= 0.7f;
-            ani.SetBool("IsRight", true);
-            ani.SetBool("IsRightRoll", isRoll);
-        }
-        else if (Input.GetKeyUp(KeyCode.D) || vAxis != 0f)
-        {
-            //moveDir *= 1f;
-            ani.SetBool("IsRight", false);
-        }
-        ani.SetFloat("vAxis", vAxis, 0.2f, Time.deltaTime);
-        ani.SetBool("IsRun", isRun && !isBack);
-        ani.SetBool("IsRoll", isRoll && !isBack);
-        ani.SetBool("IsBackRoll", isRoll && isBack);
         characterBody.forward = lookForward;
         transform.position += moveDir * Time.deltaTime * 5f;
+    }
+    void Animation()
+    {
+        ani.SetBool("IsLeft", isLeft && vAxis == 0f);
+        ani.SetBool("IsRight", isRight && vAxis == 0f);
+        ani.SetFloat("vAxis", vAxis, 0.2f, Time.deltaTime);
+        ani.SetBool("IsRun", isRun && !isBack);
+        if (isRoll && isGround)
+        {
+            ani.SetTrigger("IsRoll");
+        }
     }
     private void Jump()
     {
