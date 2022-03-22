623040f4dc7d6bb71c39e0bf5c5f7a5fda673ecf
[Github <[Github>
2022-03-22 13:10:11 +0900

    1

--- Assets/Quest/Scripts/Quest/Reward/PointReward.cs
+++ Assets/Quest/Scripts/Quest/Reward/PointReward.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Quest/Reward/Point", fileName = "PointReward_")]
-public class PointReward : Reward
+[CreateAssetMenu(menuName = "Quest/Reward/Gold", fileName = "GoldReward_")]
+public class GoldReward : Reward
 {
+    PlayerStatus playerStatus;
+    private void Awake() {
+        playerStatus = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStatus>();
+    }
     public override void Give(Quest quest)
     {
         //GameSystem.Instance.AddScore(Quantity);
-        PlayerPrefs.SetInt("bonusScore", Quantity);
-        PlayerPrefs.Save();
+        playerStatus.gold += Quantity;
     }
 }
