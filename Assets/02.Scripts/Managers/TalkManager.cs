8dc569fd7c6520cc93a5c50664a9506f2b7e399a
[Github <[Github>
2022-03-23 14:53:49 +0900

    퀘스트

--- Assets/02.Scripts/Managers/TalkManager.cs
+++ Assets/02.Scripts/Managers/TalkManager.cs
@@ -24,6 +24,7 @@ public class TalkManager
         talkData.Add(1, new string[] { "전사의 길을 걷겠는가?", "쉽지 않은 길일거야.."} );
         talkData.Add(2, new string[] { "마법사의 길을 걷겠는가?", "쉽지 않은 길일거야.." });
         talkData.Add(3, new string[] { "궁수의 길을 걷겠는가?", "쉽지 않은 길일거야.." });
+        talkData.Add(5, new string[] { "튜토리얼 퀘스트" });
         errorString = new string[] { "자네는 이미 직업이 있군"/*, "다시 한번 생각하고 오게나.."*/ };
     }
 
